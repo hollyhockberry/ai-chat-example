a884f3d52dd5b3abfe6d0f1dc4a5920c64d54dc0
hollyhockberry <49336378+hollyhockberry@users.noreply.github.com>
2023-07-20 07:57:57 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# ai-chat-example
+ 
+ChatGPTの実験
+
+## 準備
+
+APIキーの設定
+
+```sh
+echo "export OPENAI_API_KEY='yourkey'" >> ~/.zshrc
+```
+
+参考: [Best Practices for API Key Safety](https://help.openai.com/en/articles/5112595-best-practices-for-api-key-safety)
+
+必要であればWiFiのSSIDとパスフレーズをそれぞれ```WIFI_SSID```と```WIFI_PSK```に設定します。  
+(未設定の場合は接続済みのAPに接続します)
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+// Copyright (c) 2023 Inaba (@hollyhockberry)
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+
+#include <M5Unified.h>
+#include <ChatClient.h>
+
+namespace {
+constexpr const char* OPENAI_ROOT_CA = \
+  "-----BEGIN CERTIFICATE-----\n" \
+  "MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ\n" \
+  "RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD\n" \
+  "VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX\n" \
+  "DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y\n" \
+  "ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy\n" \
+  "VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr\n" \
+  "mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr\n" \
+  "IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK\n" \
+  "mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu\n" \
+  "XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy\n" \
+  "dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye\n" \
+  "jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1\n" \
+  "BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3\n" \
+  "DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92\n" \
+  "9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx\n" \
+  "jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0\n" \
+  "Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz\n" \
+  "ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS\n" \
+  "R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp\n" \
+  "-----END CERTIFICATE-----\n";
+
+ChatClient _client(OPENAI_API_KEY, OPENAI_ROOT_CA);
+String _message = "";
+
+}  // namespace
+
+void setup() {
+  M5.begin();
+
+  if (WIFI_SSID == "" || WIFI_PSK == "") {
+    WiFi.begin();
+  } else {
+    WiFi.begin(WIFI_SSID, WIFI_PSK);
+  }
+  while (WiFi.status() != WL_CONNECTED) {
+    ::delay(1000);
+    M5_LOGI("Connecting to WiFi...");
+  }
+  M5_LOGI("Connected to WiFi");
+
+  _client.begin();
+  _message = "";
+}
+
+void loop() {
+  const auto ch = Serial.read();
+  String res;
+  if (ch >= 0) {
+    switch (ch) {
+    case '\n':
+    case '\r':
+      if (!_message.isEmpty()) {
+        Serial.printf("You: %s\r\n", _message.c_str());
+        if (_client.Chat(_message.c_str(), res)) {
+          Serial.printf("GPT: %s\r\n", res.c_str());
+        }
+        _message.clear();
+      }
+      break;
+    default:
+      _message += (char)ch;
+      break;
+    }
+  }
+}
